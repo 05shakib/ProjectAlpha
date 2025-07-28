5b47c12eaddde6efe539162431b3a6d811f62814
Md. Nazmul Islam Shakib <05shakib@gmail.com>
2025-07-29 05:50:53 +0600

    In

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function App() {
   return (
     <div className="app-container">
-      {/* Your app content goes here */}
       <h1>Welcome to ProjectAlpha</h1>
       {/* Add your routing or main components here */}
     </div>
